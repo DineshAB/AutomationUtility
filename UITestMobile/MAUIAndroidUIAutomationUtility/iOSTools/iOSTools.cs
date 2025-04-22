3ac86929b08b60e798321b0a0ad3596eca46646b
aravindkannanarayanarao <92799581+aravindkannanarayanarao@users.noreply.github.com>
2025-04-22 16:31:15 +0530

    Update iOSTools.cs

--- UITestMobile/MAUIAndroidUIAutomationUtility/iOSTools/iOSTools.cs
+++ UITestMobile/MAUIAndroidUIAutomationUtility/iOSTools/iOSTools.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using MAUIAndroidUIAutomationUtility.Helper;
 
@@ -67,5 +70,69 @@ namespace MAUIAndroidUIAutomationUtility.iOSTools
             }
         }
 
+        public static string ListDevices()
+        {
+            try
+            {
+                string resultJson = "xcrun simctl list devices --json";
+                CommondExcecute.ExecuteCommand(resultJson);
+                // Deserialize JSON data into SimulatorDevices object
+                var result = JsonSerializer.Deserialize<SimulatorDevices>(resultJson);
+
+                if (result?.Devices is null || result.Devices.Count == 0)
+                {
+                    return "No simulator devices available.";
+                }
+
+                // Prepare table header
+                var devicesTable = new StringBuilder("# Simulator Devices\n\n");
+                devicesTable.AppendLine("| Name             | Udid                | Runtime       |");
+                devicesTable.AppendLine("|------------------|---------------------|---------------|");
+
+                // Process devices and format table rows
+                foreach (var runtime in result.Devices)
+                {
+                    string runtimeName = runtime.Key.Replace("com.apple.CoreSimulator.SimRuntime.", string.Empty);
+
+                    foreach (var device in runtime.Value)
+                    {
+                        device.Runtime = runtimeName;
+                        devicesTable.AppendLine($"| {device.Name,-16} | {device.Udid,-20} | {runtimeName,-13} |");
+                    }
+                }
+
+                return devicesTable.ToString();
+            }
+            catch (JsonException jsonEx)
+            {
+                return $"Error parsing simulator devices: {jsonEx.Message}";
+            }
+            catch (Exception ex)
+            {
+                return $"Error retrieving simulator devices: {ex.Message}";
+            }
+        }
+
     }
+}
+
+public class SimulatorDevices
+{
+    [JsonPropertyName("devices")]
+    public Dictionary<string, List<SimulatorDevice>> Devices { get; set; }
+}
+
+public class SimulatorDevice
+{
+    [JsonPropertyName("udid")]
+    public string Udid { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("deviceTypeIdentifier")]
+    public string Runtime { get; set; }
+
+    [JsonPropertyName("state")]
+    public string State { get; set; }
 }
