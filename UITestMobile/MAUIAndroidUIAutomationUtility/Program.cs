8ece556c8e9b6883ce3fcab0044fa8ed045b5530
aravindkannanarayanarao <92799581+aravindkannanarayanarao@users.noreply.github.com>
2025-04-23 11:29:18 +0530

    Update Program.cs

--- UITestMobile/MAUIAndroidUIAutomationUtility/Program.cs
+++ UITestMobile/MAUIAndroidUIAutomationUtility/Program.cs
@@ -9,15 +9,25 @@ class Program
 {
     static void Main()
     {
-        string documentFolder = "/Users/aravindkannanarayanarao/Documents";
+        string documentFolder= "/Users/aravindkannanarayanarao/Documents";
         // Appium 1
         List<Dictionary<string, string>> projects = new List<Dictionary<string, string>>
         {
-        //new Dictionary<string, string> { { "ProjectName", "maui-chat-tests" }, { "SampleName", "SfChatSample" }, { "ApplicationID", "com.companyname.sfchatsample" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5_API_33" } },
-        //new Dictionary<string, string> { { "ProjectName", "BusyIndicator-MAUI-tests" }, { "SampleName", "SfBusyIndicatorSample" }, { "ApplicationID", "com.companyname.SfBusyIndicatorSample" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_2_XL_API_28" } },
+
+        //Android platform
+
+        new Dictionary<string, string> { { "ProjectName", "maui-chat-tests" }, { "SampleName", "SfChatSample" }, { "ApplicationID", "com.companyname.sfchatsample" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5_API_33" } },
+        new Dictionary<string, string> { { "ProjectName", "BusyIndicator-MAUI-tests" }, { "SampleName", "SfBusyIndicatorSample" }, { "ApplicationID", "com.companyname.SfBusyIndicatorSample" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_2_XL_API_28" } },
         
-        new Dictionary<string, string> { { "ProjectName", "maui-chat-tests" }, { "SampleName", "SfChatSample" }, { "ApplicationID", "com.companyname.sfchatsample" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } },
-        new Dictionary<string, string> { { "ProjectName", "BusyIndicator-MAUI-tests" }, { "SampleName", "SfBusyIndicatorSample" }, { "ApplicationID", "com.companyname.SfBusyIndicatorSample" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } },
+        //iOS platform
+
+         new Dictionary<string, string> { { "ProjectName", "maui-chat-tests" }, { "SampleName", "SfChatSample" }, { "ApplicationID", "com.companyname.sfchatsample" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } },
+         new Dictionary<string, string> { { "ProjectName", "BusyIndicator-MAUI-tests" }, { "SampleName", "SfBusyIndicatorSample" }, { "ApplicationID", "com.companyname.SfBusyIndicatorSample" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } },
+        
+        //MacCatalyst platform
+
+        //new Dictionary<string, string> { { "ProjectName", "maui-chat-tests" }, { "SampleName", "SfChatSample" }, { "ApplicationID", "com.companyname.sfchatsample" }, { "Platform", "UITests.macOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } }
+        //new Dictionary<string, string> { { "ProjectName", "BusyIndicator-MAUI-tests" }, { "SampleName", "SfBusyIndicatorSample" }, { "ApplicationID", "com.companyname.SfBusyIndicatorSample" }, { "Platform", "UITests.macOS" }, { "EmulatorCommand", "A345178C-6D96-4B7E-83BD-266E3B81B0F7" } },
         };
 
         foreach (var project in projects)
@@ -75,8 +85,8 @@ class Program
         {
             var iphone13promax = "815F3742-98E7-4405-9611-EC74A30DB5F2";
             string emulator = $"{project["EmulatorCommand"]}";
-            string appPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["SampleName"]}";
-            string testPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["Platform"]}";
+            string appPath = $"{documentFolder}/AppiumiOS/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["SampleName"]}";
+            string testPath = $"{documentFolder}/AppiumiOS/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["Platform"]}";
             string TestRun = $"dotnet test {testPath}";
             Console.WriteLine($"Running commands for {project["ProjectName"]}...");
 
@@ -87,6 +97,21 @@ class Program
 
             Console.WriteLine($"UITest started for project : {project["SampleName"]} Sample : {project["SampleName"]} Platform : {project["Platform"]} ");
             CommondExcecute.ExecuteCommand(TestRun);
+        }
+        else if( project["Platform"] == "UITests.macOS")
+        {
+            string appPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["SampleName"]}";
+            string testPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["Platform"]}";
+            string publishCommand = "dotnet publish -f net9.0-maccatalyst -c Release";
+            string TestRun = $"dotnet test {testPath}";
+            string pkgInstaller = $"sudo -S installer -pkg {appPath}/bin/Release/net9.0-maccatalyst/publish/{project["SampleName"]}-1.0.pkg -target /";
+            Console.WriteLine($"Running commands for {project["ProjectName"]}...");
+            Console.WriteLine($"Starting {project["SampleName"]} build and publish");
+            CommondExcecute.ExecuteCommand($"cd {appPath} && {publishCommand}");
+            Console.WriteLine($"Installing {project["SampleName"]} in to simulator");
+            CommondExcecute.ExecuteCommand($"echo 'Syncfusion#132' | {pkgInstaller}");
+            Console.WriteLine($"UITest started for project : {project["SampleName"]} Sample : {project["SampleName"]} Platform : {project["Platform"]} ");
+            CommondExcecute.ExecuteCommand($"cd {testPath} && {TestRun}");
         }
     }
 }
